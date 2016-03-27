5dc01397a632f2755524301f4817cfcee08d497f
Jon Ander Romero Martínez <jonanderromero@gmail.com>
2016-03-27 12:38:15 +0100

    First flask template rendering.

--- vagrant/restaurant_menu/project.py
+++ vagrant/restaurant_menu/project.py
@@ -1,11 +1,36 @@
-from flask import Flask
+from flask import Flask, render_template
+from database_helper import db_init
+from database_setup import Restaurant, MenuItem
 app = Flask(__name__)
+session = db_init()
 
 
 @app.route('/')
-@app.route('/hello')
-def HelloWorld():
-    return 'Hello World!'
+@app.route('/restaurants/<int:restaurant_id>/menu/')
+def restaurantMenu(restaurant_id):
+    restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
+    items = session.query(MenuItem).filter_by(restaurant_id=restaurant.id)
+    return render_template('menu.html',
+                           restaurant=restaurant,
+                           items=items)
+
+
+# Task 1: Create route for newMenuItem function here
+@app.route('/restaurants/<int:restaurant_id>/menu/item/new')
+def newMenuItem(restaurant_id):
+    return "page to create a new menu item. Task 1 complete!"
+
+
+# Task 2: Create route for editMenuItem function here
+@app.route('/restaurants/<int:restaurant_id>/menu/item/<int:item_id>/edit')
+def editMenuItem(restaurant_id, item_id):
+    return "page to edit a menu item. Task 2 complete!"
+
+
+# Task 3: Create a route for deleteMenuItem function here
+@app.route('/restaurants/<int:restaurant_id>/menu/item/<int:item_id>/delete')
+def deleteMenuItem(restaurant_id, item_id):
+    return "page to delete a menu item. Task 3 complete!"
 
 
 if __name__ == '__main__':
